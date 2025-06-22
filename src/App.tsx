856d9814305b9fe8b520af79646b4dbc121f0ab2
Jonathan Favorel <35894271+jonathanFavorel@users.noreply.github.com>
2025-06-23 00:50:18 +0200

    fix: correction de la condition d'affichage du fond d'écran

--- src/App.tsx
+++ src/App.tsx
@@ -94,21 +94,20 @@ const Portfolio = () => {
     <StyledThemeProvider theme={currentTheme}>
       <CustomCursor />
       <GlobalStyles />
-      {activeBackgroundOptions &&
-        activeBackgroundOptions.particles?.number?.value > 0 && (
-          <div
-            style={{
-              position: "fixed",
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              zIndex: -1,
-            }}
-          >
-            <AnimatedBackground options={activeBackgroundOptions} />
-          </div>
-        )}
+      {activeBackgroundOptions && activeBackgroundOptions.fullScreen && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: -1,
+          }}
+        >
+          <AnimatedBackground options={activeBackgroundOptions} />
+        </div>
+      )}
       <Header />
       <main>
         <Home
